4ab9d0c6fad87a1d015cec3d88f59a5db42f3efc
Dvso67 <davit.dallakyan1.y@tumo.org>
2022-03-26 10:56:38 +0400

    das6

--- Final Project/server.js
+++ Final Project/server.js
@@ -10,3 +10,69 @@ app.get('/', function (req, res) {
     res.redirect('index.html');
 });
 server.listen(3000);
+
+
+function rand(array){
+	let i = Math.floor(Math.random()*array.length)
+	return array[i]
+}
+var matrix = []
+function matrixGen(n, gr, grEat, predator,boy,girl){
+	for(let x = 0; x < n; x++){
+		matrix[x] = []
+		for(let y = 0; y < n; y++){
+			matrix[x][y] = 0
+		}
+	}
+
+	for(let i = 0; i < gr; i++){
+		let x = Math.floor(Math.random() * n)
+		let y = Math.floor(Math.random() * n)
+
+		if(matrix[x][y]==0){
+			matrix[x][y]=1
+		}else{
+			i--
+		}
+	}
+
+	for(let i = 0; i < grEat; i++){
+		let x = Math.floor(Math.random() * n)
+		let y = Math.floor(Math.random() * n)
+		if(matrix[x][y]==0){
+			matrix[x][y]=2
+		}else{
+			i--
+		}
+	}
+
+	for(let i = 0; i < predator; i++){
+		let x = Math.floor(Math.random() * n)
+		let y = Math.floor(Math.random() * n)
+		if(matrix[x][y]==0){
+			matrix[x][y]=3
+		}else{
+			i--
+		}
+	}
+
+	for(let i = 0; i < boy; i++){
+		let x = Math.floor(Math.random() * n)
+		let y = Math.floor(Math.random() * n)
+		if(matrix[x][y]==0){
+			matrix[x][y]=4
+		}else{
+			i--
+		}
+	}
+	for(let i = 0; i < girl; i++){
+		let x = Math.floor(Math.random() * n)
+		let y = Math.floor(Math.random() * n)
+		if(matrix[x][y]==0){
+			matrix[x][y]=5
+		}else{
+			i--
+		}
+	}
+}
+/*----------------------------------------------*/matrixGen(100,5100,2000,600,400,400)/*------------------------------*/
